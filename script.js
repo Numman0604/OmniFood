613bfce3c6f709f23c488c2d3bd19bd8b3c9410f
Numman0604 <nummansharif1234@gmail.com>
2025-06-28 10:28:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector(".btn-mobile");
 const headerEl = document.querySelector(".header");
 
+btnNavEl.addEventListener('click',function(){
+  headerEl.classList.toggle("nav-open");
+})
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
