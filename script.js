c193b215794f9bbd1e2b52a766ef563968d42636
Numman0604 <nummansharif1234@gmail.com>
2025-06-28 10:59:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,13 @@ allLinks.forEach(function(link){
   link.addEventListener('click', function(e){
     e.preventDefault();
     const href =link.getAttribute('href');
-    console.log(href);
 
     if(href === "#")window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+
+    if(href !== "#" && href.startsWith("#"))
   })
 })
 
